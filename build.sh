c213a7720626478573783fb43ce61e3b6e3e9747
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-02-11 12:48:08 +0100

    dev repo

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt install build-essential git pkg-config libgl1-mesa-dev libpthread-stubs0-dev libjpeg-dev libxml2-dev libpng-dev libtiff5-dev libgdal-dev libpoppler-dev libdcmtk-dev libgstreamer1.0-dev libgtk2.0-dev libcairo2-dev libpoppler-glib-dev libxrandr-dev libxinerama-dev curl cmake
 
-git clone https://github.com/esmini/esmini.git esmini
+git clone git@github.com:ebadi/esmini.git esmini # dev repo
 cd esmini
-git checkout b772909dae9205aaacccd2692dc42599888afa57 # 1st Feb 2021 = esmini 2.1.5 (build 1108)
+git checkout dev # dev branch
 mkdir build
 cd build
 cmake ../ -DUSE_OSG=true -DCMAKE_BUILD_TYPE=Release
